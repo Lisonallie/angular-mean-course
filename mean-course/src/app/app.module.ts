49716de487a7b07ef7729f60503259dab2280f86
Lisonallie <allisonvanlinden@gmail.com>
2019-12-19 17:13:32 +0100

    add angular material

--- mean-course/src/app/app.module.ts
+++ mean-course/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './post-create/post-create.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { PostCreateComponent } from './post-create/post-create.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
