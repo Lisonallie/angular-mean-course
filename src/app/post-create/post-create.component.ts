f063a1b4f118b31345637d6f6a5794f40674f043
Lisonallie <allisonvanlinden@gmail.com>
2019-12-20 12:08:10 +0100

    require fields

--- src/app/post-create/post-create.component.ts
+++ src/app/post-create/post-create.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Post } from '../post.model';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-post-create',
@@ -17,8 +18,9 @@ export class PostCreateComponent implements OnInit {
   ngOnInit() {
   }
 
-  onAddPost() {
-    const post: Post = {title: this.enteredTitle, content: this.enteredContent}; //FOr Post here you don't need to specify that it's an array.
+  onAddPost(form: NgForm) { //NgForm is a new imported type that the behind the scenes angular uses for forms
+    const post: Post = {title: form.value.title, content: form.value.content}; //FOr Post here you don't need to specify that it's an array.
+                              //These new declarations instead of giving the inputs their own functions, access the form directly through the ngModel names
     this.postCreated.emit(post); //important to bind post as argument to it so it will output the title and content.
   }
 
