631baeeb02f12c37cce747d427f3d63d22e56f68
Lisonallie <allisonvanlinden@gmail.com>
2019-12-20 12:42:47 +0100

    copy array to getposts method

--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { Post } from './post.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostsService {
+  private posts: Post[] = []; //declare posts class exportation
 
   constructor() { }
+
+  getPosts() {
+    return [...this.posts]; //copy array here
+  }
 }
