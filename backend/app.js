b2c186e101e3abd6b431b08a72c80e2c82e74890
Lisonallie <allisonvanlinden@gmail.com>
2019-12-23 15:31:45 +0100

    fetch data from database

--- backend/app.js
+++ backend/app.js
@@ -2,10 +2,10 @@ const express = require("express");
 
 const bodyParser = require("body-parser");
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 //convention to capitalize
-const Post = require('./models/post');
+const Post = require("./models/post");
 
 //chain of middlewares (funnel send express & have different parts & each part does something different w/ the request)
 const app = express();
@@ -17,22 +17,25 @@ const app = express();
 //     next();
 // });
 
-mongoose.connect("mongodb+srv://Lisonallie:2UN7QJe79IIs5vTV@angular-posts-4ssd5.mongodb.net/test?retryWrites=true&w=majority")
-//gives feedback in nodemon whether the database connected or not
-    .then(() => {
-        console.log('connected to database');
-    })
-    .catch(() => {
-        console.log('connection failed');
-    });
-
+//                                                                                          vv change from test to another name and database with this name will be created for you
+mongoose
+  .connect(
+    "mongodb+srv://Lisonallie:2UN7QJe79IIs5vTV@angular-posts-4ssd5.mongodb.net/node-angular?retryWrites=true&w=majority"
+  )
+  //gives feedback in nodemon whether the database connected or not
+  .then(() => {
+    console.log("connected to database");
+  })
+  .catch(() => {
+    console.log("connection failed");
+  });
 
 //returns a valid express middleware for parsing json data
 //Definitely want this above our POST method & is nice before headers
 app.use(bodyParser.json());
 
 //parses a different kind of body
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
 //one additional middleware that adds CORS header
 app.use((request, response, next) => {
@@ -62,13 +65,16 @@ app.post("/api/posts", (request, response, next) => {
   //node express package, parses incoming request bodies, extracts the request data stream & converts it to data object we can use, then re-adds it on a special property to the request object.
   // old code: const post = request.body;
   const post = new Post({
-      title: request.body.title,
-      content: request.body.content,
+    title: request.body.title,
+    content: request.body.content
   });
-  console.log(post);
+  //save method provided by mongoose package for every model created with it
+  //automatically created the right query for the database & injects it into db
+  // name of collection always the plural name of your model name. in our case: Post so it's posts. mongoose does it for you
+  post.save();
   //typical status code for everything is ok, new resource was created
   response.status(201).json({
-      message: 'post added'
+    message: "post added"
   });
   //can't add next() here because we are already sending a response so would get an error
 });
@@ -76,24 +82,27 @@ app.post("/api/posts", (request, response, next) => {
 //does something with the response from 1st next function
 //also ends response writing stream & returns this response
 app.get("/api/posts", (request, response, next) => {
-  const posts = [
-    {
-      id: "fad12421l",
-      title: "first server-side post",
-      content: "this is coming from the server"
-    },
-
-    {
-      id: "ksajflaj132",
-      title: "second server-side post",
-      content: "this is coming from the server!"
-    }
-  ];
-  //can send posts or a more complicated method::
-  response.status(200).json({
-    message: "posts fetched successfully!",
-    posts: posts
+  //Want to fetch data from the posts collection
+  Post.find().then(documents => {
+    //can send posts or a more complicated method::
+    response.status(200).json({
+      message: "posts fetched successfully!",
+      posts: documents
+    });
   });
+  //   const posts = [
+  //     {
+  //       id: "fad12421l",
+  //       title: "first server-side post",
+  //       content: "this is coming from the server"
+  //     },
+
+  //     {
+  //       id: "ksajflaj132",
+  //       title: "second server-side post",
+  //       content: "this is coming from the server!"
+  //     }
+  //   ];
   //response.send('hello friend');
 });
 
