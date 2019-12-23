69a01b41702cb70e90e619a483f1e12523300c30
Lisonallie <allisonvanlinden@gmail.com>
2019-12-23 14:16:53 +0100

    add basic schema for db

--- backend/models/post.js
+++ backend/models/post.js
@@ -1,1 +1,11 @@
 const mongoose = require("mongoose");
+
+const postSchema = mongoose.Schema({
+    //pass a javascript object, define the fields & types of data the fields should store
+    //in nodejs to declare type it's with a capital
+    //title: String
+    //add more information:
+    title: { type: String, required: true }, //find more specifications in mongoose docs
+    content: { type: String, required: true }
+});
+
