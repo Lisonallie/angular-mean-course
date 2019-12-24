9bb2a3e6ac9bd2e276bba11643086b370540cb26
Lisonallie <allisonvanlinden@gmail.com>
2019-12-24 15:40:13 +0100

    add event clarification in typescript

--- src/app/post-create/post-create.component.ts
+++ src/app/post-create/post-create.component.ts
@@ -68,6 +68,10 @@ export class PostCreateComponent implements OnInit {
     });
   }
 
+  onImageChosen(event: Event) { //Event is a default type you dont have to import
+    const file = (event.target as HTMLInputElement).files[0] //as ... does a typescript conversion and clearly tells TS that the file is that sort of type w/ files property
+  }
+
   //onSavePost(form: NgForm) no longer valid because we no longer pass the form as an argument---we have our own form object^^^
   onSavePost() { //NgForm is a new imported type that the behind the scenes angular uses for forms
     if (this.form.invalid) { // <--- add 'this' before form.invalid (reactive form)
