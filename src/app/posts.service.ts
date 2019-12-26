90d00b83116b4d1ab81d07cc39db6d0c1a027248
Lisonallie <allisonvanlinden@gmail.com>
2019-12-26 15:08:37 +0100

    add else clause for updating post

--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -93,7 +93,7 @@ export class PostsService {
       postData.append("content", content),
       postData.append("image", image, title)
     } else {
-
+      const postData: Post = { id: id, title: title, content: content, imagePath: image}
     }
     this.http.patch("http://localhost:3000/api/posts/" + id, post)
       .subscribe(response => {
