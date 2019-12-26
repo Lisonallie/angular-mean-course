c9f27015696c1d6ffeb63a489b454d014f1ec5d3
Lisonallie <allisonvanlinden@gmail.com>
2019-12-26 10:12:18 +0100

    begin constructing mimeType function

--- src/app/post-create/mime-type.validator.ts
+++ src/app/post-create/mime-type.validator.ts
@@ -1,5 +1,14 @@
 import { AbstractControl } from '@angular/forms';
+import { Observable } from 'rxjs';
 
-export const mimeType = (control: AbstractControl) => {
-    
+//because it's an asynchronous validator, have to vvvv define special return type: either a promise or an observable
+//if a validator return null, it is valid. null is valid
+//                                                             vv saying this'll have a property of type string, no specific property name-- dynamic (of type any)
+//                                                         v both values (promise & observable) are generic so we can clarify what type of data we want to see:
+export const mimeType = (control: AbstractControl): Promise<{ [key: string]: any }> | Observable<{ [key: string]: any }> => {
+    const file = control.value as File;
+    const fileReader = new FileReader();
+    //we need onloadend not onload
+    //create our own observable because that's what needs to be returned by function:
+    const frObs = Observable.create(observer: Observer) => {};
 }
