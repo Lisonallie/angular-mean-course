ed9d2f8e81ca41d4e472fea6d5f21b28c4b4fcfa
Lisonallie <allisonvanlinden@gmail.com>
2019-12-27 17:07:49 +0100

    add createuser method

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  createUser(email: String, password: String) {
+      
+  }
 }
