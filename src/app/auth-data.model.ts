d0b785ea7a2a5cfe4ba3e2274e81b3ed2cb98eb3
Lisonallie <allisonvanlinden@gmail.com>
2019-12-27 17:14:48 +0100

    add auth data to post request

---
+++ src/app/auth-data.model.ts
@@ -0,0 +1,5 @@
+// use for submitting data to the backend
+export interface AuthData {
+    email: string;
+    password: string;
+}
--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { AuthData } from './auth-data.model';
 
 @Injectable({
   providedIn: 'root'
@@ -9,8 +10,13 @@ export class AuthService {
   //I want to send an http request so I need to inject the http client
   constructor(private http: HttpClient) { }
 
-  createUser(email: String, password: String) {
+  createUser(email: string, password: string) {
+    const authData: AuthData = {email: email, password: password};
       //send request, should go to our local domain /api/user/signup
-      this.http.post("http://localhost:3000/api/user/signup");
+      // for this request need to create a new model
+      this.http.post("http://localhost:3000/api/user/signup", authData)
+        .subscribe(response => {
+          console.log(response);
+        });
   }
 }
