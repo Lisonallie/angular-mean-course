9b33d112686883463c65ff2bc7a365e76adab6f2
Lisonallie <allisonvanlinden@gmail.com>
2019-12-27 16:42:07 +0100

    import bcrypt

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,10 +1,18 @@
 //implement login and signup routes
 const express = require("express");
+const bcrypt = require("bcrypt");
+
+const User = require("../models/users");
 
 const router = express.Router();
 
 router.post("/signup", (request, response, next) => {
     //I want to create a new user & store it in the database
+    const user = new User({
+        email: request.body.email,
+        //encrypt password (hash)
+        
+    })
 });
 
 module.exports = router;
