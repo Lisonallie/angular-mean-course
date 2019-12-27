8a6420645fe79fae25cde336d76b2f1dfa518ffd
Lisonallie <allisonvanlinden@gmail.com>
2019-12-27 17:10:06 +0100

    add request location route

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -6,9 +6,11 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AuthService {
 
+  //I want to send an http request so I need to inject the http client
   constructor(private http: HttpClient) { }
 
   createUser(email: String, password: String) {
-      
+      //send request, should go to our local domain /api/user/signup
+      this.http.post("http://localhost:3000/api/user/signup");
   }
 }
