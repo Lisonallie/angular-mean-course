3a82d31d682e5f5f29668ab2a2b4e30b2d509924
Lisonallie <allisonvanlinden@gmail.com>
2019-12-27 09:17:47 +0100

    add options of items per pege

--- src/app/post-list/post-list.component.ts
+++ src/app/post-list/post-list.component.ts
@@ -15,6 +15,7 @@ export class PostListComponent implements OnInit, OnDestroy {
   isLoading = false;
   totalPosts = 10;
   postsPerPage = 2;
+  pageSizeOptions = [1, 2, 5, 10];
 
   constructor(public postsService: PostsService) { //public keyword automatically creates a new property in this component & stores the incoming value in that property
     
