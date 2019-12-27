190f7e06d68e7b5588dd15a0819b6c67312070dc
Lisonallie <allisonvanlinden@gmail.com>
2019-12-27 17:03:04 +0100

    add comments to hashing password & adding to database

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -10,12 +10,14 @@ router.post("/signup", (request, response, next) => {
     //encrypt password (hash)
     //          v what to hash,       v salting round, higher number, longer it'll take but safer it'll be
     bcrypt.hash(request.body.password, 10)
+    //.then is a promise, yields us the hash once it's done
         .then(hash => {
             //I want to create a new user & store it in the database
             const user = new User({
                 email: request.body.email,
                 password: hash  
             });
+            //     vv save the user to the database
             user.save()
                 .then(result => {
                     response.status(201).json({
@@ -23,8 +25,10 @@ router.post("/signup", (request, response, next) => {
                         result: result
                     });
                 })
+                //to show what happens when we try to add a user whose email is already stored in the database
                 .catch(error => {
                     result.status(500).json({
+                        //      vv holds the error I'm getting
                         error: error
                     });
                 });
