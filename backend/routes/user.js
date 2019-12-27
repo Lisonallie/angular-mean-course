7ed5c521edc597bac71c217a763141b64de478f7
Lisonallie <allisonvanlinden@gmail.com>
2019-12-27 16:33:45 +0100

    add new user mongoose schema

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,3 +2,9 @@
 const express = require("express");
 
 const router = express.Router();
+
+router.post("/signup", (request, response, next) => {
+    //I want to create a new user & store it in the database
+});
+
+module.exports = router;
