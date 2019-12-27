9a7385e7db4ea442f31495da707d64b7b69d071c
Lisonallie <allisonvanlinden@gmail.com>
2019-12-27 16:37:53 +0100

    add unique email validator

--- backend/models/users.js
+++ backend/models/users.js
@@ -1,8 +1,12 @@
 const mongoose = require("mongoose");
+const uniqueValidator = require('mongoose-unique-validator');
 
 const userSchema = mongoose.Schema({
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true }
 });
+
+//Add unique validator plugin to get an error if it tries to save a user with an email that already exists
+userSchema.plugin(uniqueValidator);
 
 module.exports = mongoose.model('User', userSchema);
