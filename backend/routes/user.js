399fa213dc6b145b1563de27b4f24affbab69d22
Lisonallie <allisonvanlinden@gmail.com>
2019-12-27 16:54:43 +0100

    add functionality to create user & store in database

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -7,12 +7,28 @@ const User = require("../models/users");
 const router = express.Router();
 
 router.post("/signup", (request, response, next) => {
-    //I want to create a new user & store it in the database
-    const user = new User({
-        email: request.body.email,
-        //encrypt password (hash)
-        
-    })
+    //encrypt password (hash)
+    //          v what to hash,       v salting round, higher number, longer it'll take but safer it'll be
+    bcrypt.hash(request.body.password, 10)
+        .then(hash => {
+            //I want to create a new user & store it in the database
+            const user = new User({
+                email: request.body.email,
+                password: hash  
+            });
+            user.save()
+                .then(result => {
+                    response.status(201).json({
+                        message: "User created",
+                        result: result
+                    });
+                })
+                .catch(error => {
+                    result.status(500).json({
+                        error: error
+                    });
+                });
+        });
 });
 
 module.exports = router;
