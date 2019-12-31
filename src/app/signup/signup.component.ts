39293e08fff1620afe8dbf596e2d43360d8fccda
Lisonallie <allisonvanlinden@gmail.com>
2019-12-31 09:12:00 +0100

    add create user method on sign up

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-signup',
@@ -9,13 +10,18 @@ import { NgForm } from '@angular/forms';
 export class SignupComponent implements OnInit {
   isLoading = false;
 
-  constructor() { }
+  constructor(public authService: AuthService) {
+
+  }
 
   ngOnInit() {
   }
 
   onSignUp(form: NgForm) {
     //send a request to the backend to add a new user
-    
+    if (form.invalid) {
+      return;
+    }
+    this.authService.createUser(form.value.email, form.value.password);
   }
 }
