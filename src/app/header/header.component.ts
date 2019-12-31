7f9bead7bc275e706b2652308b3240435d5725f7
Lisonallie <allisonvanlinden@gmail.com>
2019-12-31 14:50:04 +0100

    add basic subscribe & unsubscribe to header comp

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,16 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { AuthService } from '../auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
+  private authListenerSubs: Subscription;
 
   constructor(private authService: AuthService) {}
   //use auth service to get the token we need
   ngOnInit() {
+    //set up subscription to the authStatusListener in auth.service.js
+    this.authListenerSubs = this.authService.getAuthStatusListener().subscribe();
+  }
+
+  ngOnDestroy() {
+    this.authListenerSubs.unsubscribe();
   }
 
 }
