4be4c8450519b39cab57270c3c01bcefeea1b4aa
Lisonallie <allisonvanlinden@gmail.com>
2019-12-31 13:26:09 +0100

    import jsonwebtoken

--- backend/middleware/check-auth.js
+++ backend/middleware/check-auth.js
@@ -1,1 +1,6 @@
 //check whether authenticated or not
+// if (a) you do have a token attached to your request but that doesn't mean the token is valid
+// so we validate this token
+
+const jwt = require('jsonwebtoken');
+
