71baeeaecb404faf40eb4a004dab5197a4307cd4
Lisonallie <allisonvanlinden@gmail.com>
2019-12-31 13:53:44 +0100

    create interceptor file

---
+++ src/app/auth-interceptor.ts
@@ -0,0 +1,2 @@
+//interceptor is official feature provided by the angular http client 
+//functions that will run on any outgoing http request, can then manipulate these requests, ex: attaching our token
