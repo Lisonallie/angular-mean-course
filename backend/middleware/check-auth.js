4f95af65e5c285b352057ceb71d8fc99ab82e49d
Lisonallie <allisonvanlinden@gmail.com>
2019-12-31 13:34:36 +0100

    add try & catch to authorization process

--- backend/middleware/check-auth.js
+++ backend/middleware/check-auth.js
@@ -2,14 +2,23 @@
 // if (a) you do have a token attached to your request but that doesn't mean the token is valid
 // so we validate this token
 
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 module.exports = (request, response, next) => {
-//function that gets executed on the request
+    //will try to execute this but it can fail
+  try {
+    //function that gets executed on the request
     //attach authorization info to a request, stored in headers
     //                                          vv "Bearer" is commonality string used, use split to get the 1st instance of it
     //                                                     vv part after the white space, aka our Token
     //                                                        vv token(comment)
     //                              vv looks like "Bearer sdlfkqjldsjfqmsqdlf"
     const token = request.headers.authorization.split(" ")[1];
+  } catch (error) {  //if it fails
+    //don't have a token, not authenticated
+    response.status(401).json({
+        message: "Authorization failed"
+    })
+  }
+  
 };
