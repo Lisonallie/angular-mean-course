061542321015ce6a0996ad1d7c9e3949aaa03aef
Lisonallie <allisonvanlinden@gmail.com>
2019-12-31 13:32:06 +0100

    define token constant using authorization header

--- backend/middleware/check-auth.js
+++ backend/middleware/check-auth.js
@@ -4,3 +4,12 @@
 
 const jwt = require('jsonwebtoken');
 
+module.exports = (request, response, next) => {
+//function that gets executed on the request
+    //attach authorization info to a request, stored in headers
+    //                                          vv "Bearer" is commonality string used, use split to get the 1st instance of it
+    //                                                     vv part after the white space, aka our Token
+    //                                                        vv token(comment)
+    //                              vv looks like "Bearer sdlfkqjldsjfqmsqdlf"
+    const token = request.headers.authorization.split(" ")[1];
+};
