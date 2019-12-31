4905f05636a060ddcaf33569d64bd1db1da30c3e
Lisonallie <allisonvanlinden@gmail.com>
2019-12-31 16:06:01 +0100

    import canactivate

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -1,4 +1,6 @@
+import { CanActivate } from '@angular/router';
+
 //guarding routes, angular can have the router check the info in here before loading a route to see if it should proceed or not
-export class AuthGuard {
+export class AuthGuard implements CanActivate {
 
 }
