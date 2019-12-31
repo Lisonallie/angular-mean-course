ab417834e1ac3db6cd216b7e8a07a1fc1ebca0e7
Lisonallie <allisonvanlinden@gmail.com>
2019-12-31 16:06:33 +0100

    implement interface canactivate

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -2,5 +2,8 @@ import { CanActivate } from '@angular/router';
 
 //guarding routes, angular can have the router check the info in here before loading a route to see if it should proceed or not
 export class AuthGuard implements CanActivate {
+    canActivate(route: import("@angular/router").ActivatedRouteSnapshot, state: import("@angular/router").RouterStateSnapshot): boolean | import("@angular/router").UrlTree | import("rxjs").Observable<boolean | import("@angular/router").UrlTree> | Promise<boolean | import("@angular/router").UrlTree> {
+        throw new Error("Method not implemented.");
+    }
 
 }
