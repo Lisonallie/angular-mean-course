b5a3e73d51bf7d51a78010abce5d176065cae24a
Lisonallie <allisonvanlinden@gmail.com>
2019-12-31 16:11:02 +0100

    add auth service & injectable statement

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -1,9 +1,19 @@
 import { CanActivate } from '@angular/router';
+import { Injectable } from '@angular/core';
+import { AuthService } from './auth.service';
 
 //guarding routes, angular can have the router check the info in here before loading a route to see if it should proceed or not
+@Injectable()
 export class AuthGuard implements CanActivate {
+
+    constructor(private authService: AuthService) {}
+
+    // click on error then lightbulb and click add interface
     canActivate(route: import("@angular/router").ActivatedRouteSnapshot, state: import("@angular/router").RouterStateSnapshot): boolean | import("@angular/router").UrlTree | import("rxjs").Observable<boolean | import("@angular/router").UrlTree> | Promise<boolean | import("@angular/router").UrlTree> {
-        throw new Error("Method not implemented.");
+        // if you return true the router will say ok this route is accessible and give access
+        // if you return false the router will deny access to the route
+        // don't want to hard code true or false, but want to get the info from the auth service
+        return true;
     }
 
 }
