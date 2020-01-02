3f67a438c6274e99f76977f5008bba78feb2bab0
Lisonallie <allisonvanlinden@gmail.com>
2020-01-02 15:54:27 +0100

    return throw error in catch error

--- src/app/error-interceptor.ts
+++ src/app/error-interceptor.ts
@@ -1,13 +1,22 @@
-import { HttpInterceptor, HttpRequest, HttpHandler } from '@angular/common/http';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpErrorResponse } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 
 //interceptor is official feature provided by the angular http client 
 //functions that will run on any outgoing http request, can then manipulate these requests, ex: attaching our token
 
 export class ErrorInceptor implements HttpInterceptor {
-    constructor() {}
     //intercept method is required, angular calls it for requests leaving my app
     //interceptor works a lot like middleware just for outgoing requests instead of incoming
     intercept(request: HttpRequest<any>, next: HttpHandler) {
-        return next.handle(request);
+        return next.handle(request).pipe(
+            //allows us to handle errors emitted in this stream of requests
+            catchError((error: HttpErrorResponse) => {
+                console.log(error);
+                //need to return an observable here
+                return throwError(error);
+                
+            });
+        );
     };
 }
