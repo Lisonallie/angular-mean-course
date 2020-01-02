6d567c060a2c83fad54d270592f78b6c5421c0cf
Lisonallie <allisonvanlinden@gmail.com>
2020-01-02 10:42:45 +0100

    implement auth user upon start of the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit() {
+    this.authService.autoAuthUser();
+  }
 
   // storedPosts: Post[] = []; //Post is typescript syntax for saying we have an array of posts in there from the imported interface.
 
