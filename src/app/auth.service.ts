7a27438303850a29dadb1c8d9c1cbc0c01f56508
Lisonallie <allisonvanlinden@gmail.com>
2020-01-02 15:38:51 +0100

    assign error handler if not authenticated

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -41,6 +41,9 @@ export class AuthService {
       return this.http.post("http://localhost:3000/api/user/signup", authData)
       .subscribe(() => {
         this.router.navigate(["/"]);
+      }, error => {
+        //tell app we're not authenticated
+        this.authStatusListener.next(false);
       });
   }
 
