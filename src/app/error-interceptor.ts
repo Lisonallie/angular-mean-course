3967c32952bd1c53d601c1b7cf41baa8ffe5448b
Lisonallie <allisonvanlinden@gmail.com>
2020-01-02 16:07:51 +0100

    add injectable & matdialog

--- src/app/error-interceptor.ts
+++ src/app/error-interceptor.ts
@@ -1,13 +1,19 @@
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpErrorResponse } from '@angular/common/http';
 import { catchError } from 'rxjs/operators';
 import { throwError } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { MatDialog } from '@angular/material';
 
 //interceptor is official feature provided by the angular http client 
 //functions that will run on any outgoing http request, can then manipulate these requests, ex: attaching our token
 
+@Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
     //intercept method is required, angular calls it for requests leaving my app
     //interceptor works a lot like middleware just for outgoing requests instead of incoming
+
+    constructor(private dialog: MatDialog) {}
+
     intercept(request: HttpRequest<any>, next: HttpHandler) {
         return next.handle(request).pipe(
             //allows us to handle errors emitted in this stream of requests
