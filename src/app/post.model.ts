375fe733b3a843285e326b407ebf8a44392c2e7c
Lisonallie <allisonvanlinden@gmail.com>
2020-01-02 13:26:57 +0100

    add creator to model & implement in front-end

--- src/app/post.model.ts
+++ src/app/post.model.ts
@@ -3,6 +3,7 @@ export interface Post {
     title: string;
     content: string;
     imagePath: string;
+    creator: string;
 }
 
 //a sort of blueprint to show how a post looks like in the application (title, content)
