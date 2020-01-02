cc5249417c56a9253ef0896a4a38c7c69bc6d90c
Lisonallie <allisonvanlinden@gmail.com>
2020-01-02 15:35:10 +0100

    set up code for subscribing & unsubscribing

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,18 +1,22 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, OnDestroy } from "@angular/core";
 import { NgForm } from "@angular/forms";
 import { AuthService } from "../auth.service";
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: "app-signup",
   templateUrl: "./signup.component.html",
   styleUrls: ["./signup.component.css"]
 })
-export class SignupComponent implements OnInit {
+export class SignupComponent implements OnInit, OnDestroy {
   isLoading = false;
+  private authStatusSub: Subscription;
 
   constructor(public authService: AuthService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.authService.getAuthStatusListener().subscribe();
+  }
 
   onSignUp(form: NgForm) {
     //send a request to the backend to add a new user
@@ -22,5 +26,9 @@ export class SignupComponent implements OnInit {
     this.isLoading = true;
     this.authService
       .createUser(form.value.email, form.value.password);
+  }
+
+  ngOnDestroy() {
+    this.authStatusSub.unsubscribe();
   }
 }
