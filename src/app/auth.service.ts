f879e94e1bbee26d031612c7a46fa399cd016d10
Lisonallie <allisonvanlinden@gmail.com>
2020-01-02 13:16:22 +0100

    add userId to save auth data

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -113,12 +113,13 @@ export class AuthService {
   }
 
   //store data in the local storage once authenticated
-  private saveAuthData(token: string, expirationDate: Date) {
+  private saveAuthData(token: string, expirationDate: Date, userId: string) {
     //localstorage API I can access
     //key & value pairs
     localStorage.setItem('token', token);
     //                                                vvv serialized & standardized version of the string
     localStorage.setItem('expiration', expirationDate.toISOString());
+    localStorage.setItem('userId', userId);
   }
 
   private clearAuthData() {
