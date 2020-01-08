5a5f652d12fad3607d8c2e4af8bacbaa34244afe
Lisonallie <allisonvanlinden@gmail.com>
2020-01-08 09:59:00 +0100

    add in env variable & test app functionality

--- backend/app.js
+++ backend/app.js
@@ -25,7 +25,7 @@ const app = express();
 //                                                                             vv change from test to another name and database with this name will be created for you
 mongoose
   .connect(
-    "mongodb+srv://Lisonallie:2UN7QJe79IIs5vTV@angular-posts-4ssd5.mongodb.net/node-angular?retryWrites=true&w=majority"
+    "mongodb+srv://Lisonallie:" + process.env.MONGO_ATLAS_PW + "@angular-posts-4ssd5.mongodb.net/node-angular?retryWrites=true&w=majority"
   )
   //gives feedback in nodemon whether the database connected or not
   .then(() => {
